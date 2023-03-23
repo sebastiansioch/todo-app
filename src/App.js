b98ac775e88401f9ea9c09ec725e10ea0ead38f7
Sebastian Sioch <sebastian.sioch@gmail.com>
2023-03-23 14:08:42 +0100

    add and delete task onClick

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [task, setTask] = useState("");
+  const [tasks,setTasks] = useState(["eat", "sleep"]);
+  const getTask = () => {
+    return tasks.map((task, index) => <li class="list-group-item"
+    onClick={() =>{deleteTask(index)}}>{task}</li>)
+
+  }
+  const deleteTask = (i) => {
+    const newTasks = [...tasks];
+    newTasks.splice(i, 1);
+    setTasks(newTasks);
+    
+  }
+  const addTask = (t) => {
+    if (t){
+      const newTasks = [...tasks];
+      newTasks.push(t);
+      setTasks(newTasks);
+      setTask("");
+    }
+    else{
+      alert("enter valid value")
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input class="form-control" type="text" onChange={(e) => {setTask(e.target.value)}}
+      value={task} 
+      placeholder="enter the task"></input>
+      <button class="btn btn-success w-50"onClick={()=>{addTask(task)}}>Change</button>
+      <ul class="list-group">
+        {getTask()}
+      </ul>
     </div>
   );
 }
